14b2ae4fe678b8a04ee20f17ccb9d680bd6130d4
tenagazaru8 <122034071+tenagazaru8@users.noreply.github.com>
2024-10-24 14:39:44 +0900

    upgrade

--- lib/blockcuttree.cpp
+++ lib/blockcuttree.cpp
@@ -1,237 +1,221 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-
-using namespace std;
+#include <algorithm>
 
 /*
- * block-cut-tree class
- * グラフ上の2点s,tを決めたとき，O(N+M)で全頂点に対して以下の答えを求める．
- * s-tパスであって，頂点vを経由するものが存在するかどうか？
+ * ViaVertexDetermine class
+ * 静的な無向グラフが与えられたとき，次のクエリを高速に処理する．
+ * query(s, v, t) : 単純 s-t パスであって，頂点 v を経由するものが存在するかどうか？
+ * 時間計算量: 前処理 O((|V|+|E|)log(|V|+|E|)), クエリ当たり O(1)
  */
-class blockCutTree
+class ViaVertexDetermine
 {
-  const int inf = 1 << 30;
-  int n, gnum; // 頂点数，グループの数
-  bool isdirected;
-  vector<int> eg, dep, low; // 辺のグループ番号，dfs-treeにおける各頂点の深さとlowpoint
-  vector<int> gr, apid; // 所属する成分の番号, 関節点かどうか(関節点なら成分の番号,そうでないなら-1)
-  vector<vector<pair<int, int>>> g; // グラフ, 辺に持たせる情報は(行先, 辺の番号)
-  vector<vector<int>> vgr, egr, bct, bctr;
+private:
+    std::vector<int> eg, dep, low, gr, apid, inid, dat, lookup;
+    std::vector<std::vector<std::pair<int, int>>> g;
+    std::vector<std::vector<int>> egr, bct, sp;
+    int gnum, nowdep, nowidx;
+
 public:
-  // flag=true/false -> directed/underected
-  blockCutTree(int num, const vector<pair<int, int>> &e, bool direction = false) :
-    n(num), isdirected(direction), g(num), eg(e.size(), -1), dep(num, -1), low(num, inf), gr(num, -1), apid(num, -1)
-  {
-    int siz = e.size();
-    for (int i = 0; i < siz; ++i)
+    ViaVertexDetermine(std::vector<std::pair<int, int>> e)
     {
-      g[e[i].first].push_back({e[i].second, i});
-      // 無向グラフなら逆辺を張る
-      if (!isdirected) g[e[i].second].push_back({e[i].first, i});
+        const int inf = 1 << 30;
+        {
+            int delnum = 0;
+            for (int i = 0; i < e.size(); ++i)
+            {
+                e[i - delnum] = e[i];
+                if (e[i].first == e[i].second) ++delnum;
+            }
+            while (delnum--) e.pop_back();
+            for (auto &[u, v] : e) if (u > v) std::swap(u, v);
+            std::sort(e.begin(), e.end());
+            e.erase(std::unique(e.begin(), e.end()), e.end());
+        }
+        int n = 0;
+        for (auto [u, v] : e) if (n < v) n = v;
+        ++n;
+        int siz = e.size();
+        eg.assign(siz, -1);
+        dep.assign(n, -1);
+        low.assign(n, inf);
+        gr.assign(n, -1);
+        apid.assign(n, -1);
+        g.resize(n);
+        for (int i = 0; i < siz; ++i)
+        {
+            g[e[i].first].emplace_back(e[i].second, i);
+            g[e[i].second].emplace_back(e[i].first, i);
+        }
+        gnum = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (dep[i] == -1)
+            {
+                dep[i] = 0;
+                low[i] = 0;
+                dfs1(i, -1);
+                bool isisolation = true;
+                for (auto [to, id] : g[i])
+                {
+                    if (dep[to] == 1 && eg[id] == -1)
+                    {
+                        isisolation = false;
+                        break;
+                    }
+                }
+                if (!isisolation)
+                {
+                    dfs2(i);
+                    ++gnum;
+                }
+            }
+        }
+        int apcnt = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (apid[i] != -1)
+            {
+                apid[i] = gnum + apcnt;
+                ++apcnt;
+            }
+        }
+        egr.assign(gnum, std::vector<int>());
+        bct.assign(gnum + apcnt, std::vector<int>());
+        for (int i = 0; i < siz; ++i) if (eg[i] != -1) egr[eg[i]].emplace_back(i);
+        for (int i = 0; i < gnum; ++i)
+        {
+            for (int id : egr[i])
+            {
+                if (gr[e[id].first] != i)
+                {
+                    gr[e[id].first] = i;
+                    if (apid[e[id].first] != -1)
+                    {
+                        int idx = apid[e[id].first];
+                        bct[idx].emplace_back(i);
+                        bct[i].emplace_back(idx);
+                    }
+                }
+                if (gr[e[id].second] != i)
+                {
+                    gr[e[id].second] = i;
+                    if (apid[e[id].second] != -1)
+                    {
+                        int idx = apid[e[id].second];
+                        bct[i].emplace_back(idx);
+                        bct[idx].emplace_back(i);
+                    }
+                }
+            }
+        }
+        gnum += apcnt;
+
+        nowdep = 0;
+        nowidx = 0;
+        inid.assign(gnum, -1);
+        dat.assign(2 * gnum, 1 << 30);
+        for (int i = 0; i < gnum; ++i)
+        {
+            if (inid[i] == -1)
+            {
+                buildfs(i, -1);
+                dat[nowidx++] = -inf;
+            }
+        }
+        lookup.resize(2 * gnum);
+        lookup[0] = -1;
+        for (int i = 1; i < 2 * gnum; ++i) lookup[i] = lookup[i >> 1] + 1;
+        int lumax = lookup[2 * gnum - 1];
+        sp.resize(lumax + 1);
+        sp[0] = dat;
+        for (int i = 1; i <= lumax; ++i)
+        {
+            int siz = 2 * gnum - (1 << i) + 1;
+            sp[i].resize(siz);
+            for (int j = 0; j < siz; ++j) sp[i][j] = std::min(sp[i - 1][j], sp[i - 1][j + (1 << i - 1)]);
+        }
     }
 
-    vector<int> topo = topologicalsort();
-    gnum = 0;
-    for (int i : topo)
+    bool query(int s, int v, int t)
     {
-      if (dep[i] == -1)
-      {
-        dep[i] = 0;
-        low[i] = 0;
-        dfs1(i, -1);
-        bool isisolation = true;
-        for (auto v : g[i])
-        {
-          if (dep[v.first] == 1 && eg[v.second] == -1)
-          {
-            isisolation = false;
-            break;
-          }
-        }
-        if (!isisolation)
-        {
-          dfs2(i);
-          ++gnum;
-        }
-      }
+        if (s == t) return (s == v);
+        int tmp = 0;
+        if (apid[v] != -1) tmp = 2;
+        int sid = calidx(s), vid = calidx(v), tid = calidx(t);
+        int d1 = calcdis(sid, tid), d2 = calcdis(sid, vid), d3 = calcdis(vid, tid);
+        if (d1 == -1 || d2 == -1 || d3 == -1) return false;
+        return d1 + tmp >= d2 + d3;
     }
-    
-    int apcnt = 0;
-    for (int i = 0; i < n; ++i)
-    {
-      if (apid[i] != -1)
-      {
-        apid[i] = gnum + apcnt;
-        ++apcnt;
-      }
-    }
-    egr.resize(gnum);
-    vgr.resize(gnum + apcnt);
-    bct.resize(gnum + apcnt);
-    if (isdirected) bctr.resize(gnum + apcnt);
-    for (int i = 0; i < n; ++i) if (apid[i] != -1) vgr[apid[i]].push_back(i);
-    for (int i = 0; i < siz; ++i) if (eg[i] != -1) egr[eg[i]].push_back(i);
-    for (int i = 0; i < gnum; ++i)
-    {
-      for (int id : egr[i])
-      {
-        if (gr[e[id].first] != i)
-        {
-          vgr[i].push_back(e[id].first);
-          gr[e[id].first] = i;
-          if (apid[e[id].first] != -1)
-          {
-            int idx = apid[e[id].first];
-            bct[idx].push_back(i);
-            if (isdirected) bctr[i].push_back(idx);
-            else bct[i].push_back(idx);
-          }
-        }
-        if (gr[e[id].second] != i)
-        {
-          vgr[i].push_back(e[id].second);
-          gr[e[id].second] = i;
-          if (apid[e[id].second] != -1)
-          {
-            int idx = apid[e[id].second];
-            bct[i].push_back(idx);
-            if (isdirected) bctr[idx].push_back(i);
-            else bct[idx].push_back(i);
-          }
-        }
-      }
-    }
-    gnum += apcnt;
-  }
-
-  void printparam()
-  {
-    for (int i = 0; i < n; ++i) cout << apid[i] << " \n"[i == n -1];
-    for (int i = 0; i < n; ++i) cout << dep[i] << " \n"[i == n -1];
-    for (int i = 0; i < n; ++i) cout << low[i] << " \n"[i == n -1];
-    for (int i = 0; i < vgr.size(); ++i)
-    {
-      cout << "groupnumber:" << i << endl;
-      cout << "vertices:";
-      for (auto v : vgr[i]) cout << ' ' << v;
-      cout << endl;
-      cout << "edge->";
-      for (auto v : bct[i]) cout << ' ' << v;
-      cout << endl << endl;
-    }
-  }
-
-  // パスの始点sと終点t(0-index)を渡すと，各頂点についてパスに含まれることがあるかを表すbool配列を返す．
-  // 0:含まれない，1:含まれる
-  vector<bool> isincluded(int s, int t)
-  {
-    vector<bool> ret(n);
-    s = (apid[s] == -1 ? gr[s] : apid[s]);
-    t = (apid[t] == -1 ? gr[t] : apid[t]);
-    vector<int> sdis(gnum, inf), tdis(gnum, inf);
-    sdis[s] = 0;
-    queue<int> q;
-    for (q.push(s); !q.empty(); q.pop())
-    {
-      int now = q.front();
-      for (int v : bct[now])
-      {
-        if (sdis[v] > sdis[now] + 1)
-        {
-          sdis[v] = sdis[now] + 1;
-          q.push(v);
-        }
-      }
-    }
-    vector<vector<int>> &tree = (isdirected ? bctr : bct);
-    tdis[t] = 0;
-    for (q.push(t); !q.empty(); q.pop())
-    {
-      int now = q.front();
-      for (int v : tree[now])
-      {
-        if (tdis[v] > tdis[now] + 1)
-        {
-          tdis[v] = tdis[now] + 1;
-          q.push(v);
-        }
-      }
-    }
-    if (sdis[t] < inf) for (int i = 0; i < gnum; ++i) if (sdis[i] + tdis[i] == sdis[t]) for (int v : vgr[i]) ret[v] = true;
-    return ret;
-  }
 
 private:
-  void dfs1(int now, int p)
-  {
-    int reslow = low[now];
-    for (auto v : g[now])
-    {
-      if (dep[v.first] == -1 && v.first != p)
-      {
-        dep[v.first] = dep[now] + 1;
-        low[v.first] = dep[v.first];
-        dfs1(v.first, now);
-      }
-      if (v.first != p) reslow = min(reslow, low[v.first]);
-    }
-    low[now] = reslow;
-    for (auto v : g[now])
-    {
-      if (dep[v.first] == dep[now] + 1 && v.first != p && dep[now] <= low[v.first])
-      {
-        apid[now] = 0;
-        eg[v.second] = gnum;
-        dfs2(v.first);
-        ++gnum;
-      }
-    }
-  }
 
-  void dfs2(int now)
-  {
-    for (auto v : g[now])
-    {
-      if (dep[v.first] == dep[now] + 1 && eg[v.second] == -1)
-      {
-        eg[v.second] = gnum;
-        dfs2(v.first);
-      }
-    }
-  }
+    int calidx(int id) {return apid[id] == -1 ? gr[id] : apid[id];}
 
-  vector<int> topologicalsort()
-  {
-    int idx = n;
-    vector<int> ret(n);
-    vector<bool> used(n);
-    auto topodfs = [&](auto f, int now) -> void
+    void dfs1(int now, int p)
     {
-      if (used[now]) return;
-      used[now] = true;
-      for (auto v : g[now]) f(f, v.first);
-      ret[--idx] = now;
-    };
-    for (int i = 0; i < n; ++i) topodfs(topodfs, i);
-    return ret;
-  }
+        int reslow = low[now];
+        for (auto [to, id] : g[now])
+        {
+            if (dep[to] == -1 && to != p)
+            {
+                dep[to] = dep[now] + 1;
+                low[to] = dep[to];
+                dfs1(to, now);
+            }
+            if (to != p) reslow = std::min(reslow, low[to]);
+        }
+        low[now] = reslow;
+        for (auto [to, id] : g[now])
+        {
+            if (dep[to] == dep[now] + 1 && to != p && dep[now] <= low[to])
+            {
+                apid[now] = 0;
+                eg[id] = gnum;
+                dfs2(to);
+                ++gnum;
+            }
+        }
+    }
+
+    void dfs2(int now)
+    {
+        for (auto [to, id] : g[now])
+        {
+            if (dep[to] == dep[now] + 1 && eg[id] == -1)
+            {
+                eg[id] = gnum;
+                dfs2(to);
+            }
+        }
+    }
+
+    void buildfs(int now, int p)
+    {
+        if (inid[now] == -1) inid[now] = nowidx;
+        dat[nowidx++] = nowdep;
+        for (int to : bct[now])
+        {
+            if (to == p) continue;
+            ++nowdep;
+            buildfs(to, now);
+            --nowdep;
+            dat[nowidx++] = nowdep;
+        }
+    }
+
+    int getval(int l, int r)
+    {
+        int lu = lookup[r - l + 1];
+        return std::min(sp[lu][l], sp[lu][r - (1 << lu) + 1]);
+    }
+
+    int calcdis(int s, int t)
+    {
+        int id1 = inid[s], id2 = inid[t];
+        if (id1 > id2) std::swap(id1, id2);
+        int mid = getval(id1, id2);
+        if (mid < 0) return -1;
+        return dat[id1] + dat[id2] - 2 * mid;
+    }
 };
-
-int main()
-{
-  int n, m, a, b, c;
-  cin >> n >> m >> a >> b >> c;
-  --a, --b, --c;
-  vector<pair<int, int>> e(m);
-  for (int i = 0; i < m; ++i)
-  {
-    int u, v;
-    cin >> u >> v;
-    --u, --v;
-    e[i] = {u, v};
-  }
-  blockCutTree bc(n, e);
-  vector<bool> res = bc.isincluded(a, c);
-  cout << (res[b] ? "Yes" : "No") << endl;
-}
