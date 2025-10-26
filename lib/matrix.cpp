66a6c7975125f3389b474b27be95ee90a85a2420
Shinryu <sinryu.1010.tono@gmail.com>
2025-10-26 16:27:22 +0900

    [matrix] mulの修正、エラー出力の追加

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -7,16 +7,24 @@ using namespace std;
 std::vector<std::vector<long long>> mul(const std::vector<std::vector<long long>> &v1, const std::vector<std::vector<long long>> &v2, long long mod = 1001001001001001001)
 {
     std::vector<std::vector<long long>> ans;
-    if (v1.size() != v2[0].size()) return ans;
+    if (v1[0].size() != v2.size())
+    {
+        std::cout << "matrix mul size error: (" << v1[0].size() << ", " << v2.size() << ")" << std::endl;
+        std::exit(1);
+    }
     ans.resize(v1.size(), std::vector<long long> (v2[0].size()));
-    for (int i = 0; i < v1.size(); ++i) for (int j = 0; j < v2[0].size(); ++j) for (int k = 0; k < v2.size(); ++k) ans[i][j] = (ans[i][j] + v1[i][k] * v2[k][j]) % mod;
+    for (int i = 0; i < v1.size(); ++i) for (int j = 0; j < v2[0].size(); ++j) for (int k = 0; k < v1[0].size(); ++k) ans[i][j] = (ans[i][j] + v1[i][k] * v2[k][j]) % mod;
     return ans;
 }
 
 std::vector<std::vector<long long>> pow(std::vector<std::vector<long long>> v, long long k, long long mod = 1001001001001001001)
 {
     std::vector<std::vector<long long>> ans;
-    if (v.size() != v[0].size()) return ans;
+    if (v.size() != v[0].size())
+    {
+        std::cout << "matrix pow size error: (" << v.size() << ", " << v[0].size() << ")" << std::endl;
+        std::exit(1);
+    }
     ans.resize(v.size(), std::vector<long long> (v.size()));
     for (int i = 0; i < v.size(); ++i) ans[i][i] = 1;
     for (; k > 0; k >>= 1, v = mul(v, v, mod)) if (k & 1) ans = mul(v, ans, mod);
