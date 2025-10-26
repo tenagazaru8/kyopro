3071b39afb99ad88cd35bcf85cb0705c62116dee
Shinryu <sinryu.1010.tono@gmail.com>
2025-10-26 16:27:59 +0900

    [SegTree] クラス名の修正

--- lib/segmenttree.cpp
+++ lib/segmenttree.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <stack>
 
-template <typename T> class segTree
+template <typename T> class SegTree
 {
 private:
   const T e;
@@ -11,7 +9,7 @@ private:
   std::vector<T> dat;
   T (*const eval)(T &, T &) {};
 public:
-  segTree(std::vector<T> &v, T E, T (*func)(T &, T &)) : e(E), eval(func)
+  SegTree(std::vector<T> &v, T E, T (*func)(T &, T &)) : e(E), eval(func)
   {
     int siz = static_cast<int>(v.size());
     for (num = 1; num < siz; num <<= 1);
@@ -19,7 +17,7 @@ public:
     for (int i = 0; i < siz; ++i) dat[i + num - 1] = v[i];
     for (int i = num - 2; i >= 0; --i) dat[i] = eval(dat[i * 2 + 1], dat[i * 2 + 2]);
   }
-  segTree(int n, T E, T (*func)(T &, T &)) : e(E), eval(func)
+  SegTree(int n, T E, T (*func)(T &, T &)) : e(E), eval(func)
   {
     for (num = 1; num < n; num <<= 1);
     dat = std::vector<T> (2 * num - 1, e);
@@ -42,7 +40,7 @@ public:
   }
   T getval(int left, int right)
   {
-    left = max(0, left), right = min(num, right);
+    left = std::max(0, left), right = std::min(num, right);
     T ansl = e, ansr = e;
     for (left += num - 1, right += num - 1; left < right; left >>= 1, right >>= 1)
     {
