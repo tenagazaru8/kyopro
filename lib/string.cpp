230ff799275716b007a11303d0075de83d63f372
Shinryu <sinryu.1010.tono@gmail.com>
2025-11-30 04:33:44 +0900

    [String] 引数の多様化

--- lib/string.cpp
+++ lib/string.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-vector<int> Z_algorithm(string &s)
+template<typename S>
+vector<int> Z_algorithm(const S &s)
 {
   int c = 0, n = s.size();
   vector<int> ret(n);
@@ -23,26 +24,22 @@ vector<int> Z_algorithm(string &s)
   return ret;
 }
 
-vector<int> kai(string &s)
+template<typename S>
+vector<int> kai(const S s)
 {
   int n = s.size(), c = 0;
   vector<int> ret(n);
   for (int i = 0; i < n; ++i)
   {
     int l = 2 * c - i;
-    if (c + ret[c] > i + ret[l]) ret[i] = ret[l];
+    if (l >= 0 && c + ret[c] > i + ret[l]) ret[i] = ret[l];
     else
     {
-      int j = max(c + ret[c] - i, 0);
+      int j = c + ret[c] - i;
       while (i - j >= 0 && i + j < n && s[i - j] == s[i + j]) ++j;
       ret[i] = j;
       c = i;
     }
   }
   return ret;
-}
-
-int main()
-{
-    
 }
