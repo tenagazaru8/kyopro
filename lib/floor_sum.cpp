a60e960af403bd3f828e52e20a9f795b281b42e6
Shinryu <sinryu.1010.tono@gmail.com>
2026-02-03 00:02:47 +0900

    [floor_sum] 負の値へ対応するよう修正

--- lib/floor_sum.cpp
+++ lib/floor_sum.cpp
@@ -5,12 +5,26 @@ i=0,1,...,n-1  Σfloor((a*i+b)/m)
 long long floor_sum(long long n, long long m, long long a, long long b)
 {
   long long ans = 0;
-  if (a >= m)
+  if (a < 0)
+  {
+    long long mi = a * (n - 1) + b;
+    long long d = (mi % m + m) % m;
+    mi -= d;
+    return floor_sum(n, m, -a, d) + mi / m * n;
+  }
+  else if (a >= m)
   {
     ans += (n - 1) * n * (a / m) / 2;
     a %= m;
   }
-  if (b >= m)
+  if (b < 0)
+  {
+    long long mi = b;
+    long long d = (mi % m + m) % m;
+    mi -= d;
+    return floor_sum(n, m, a, d) + mi / m * n;
+  }
+  else if (b >= m)
   {
     ans += n * (b / m);
     b %= m;
